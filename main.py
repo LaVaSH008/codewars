c9e90fa671a11a3f181d958e6e812a742daa2ae6
Danil <danrumu@gmail.com>
2022-04-20 22:25:56 +0300

    7 commit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from methods.kyu6.spin_words import spin_words
 from methods.kuy5.move_zeros import move_zeros
 from methods.kyu7.binary_array_to_number import binary_array_to_number
 from methods.kyu4.range_extraction import solution
+from methods.kyu4.snail import snail
 
 if __name__ == '__main__':
-    print(solution([-6,-3-1,3-5,7-11,14,15,17,18,19,20]))
+    print(snail([[1,2,3],
+         [4,5,6],
+         [7,8,9]]))
---
+++ methods/kyu4/snail.py
@@ -0,0 +1,4 @@
+def snail(snail_map):
+    s = []
+    s += snail_map[0]
+
