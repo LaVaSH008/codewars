ac4fad6392612ab533848c4402a95b5cfd9a4e55
Danil <danrumu@gmail.com>
2022-04-20 20:51:28 +0300

    6 commit

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from methods.kyu7.binary_array_to_number import binary_array_to_number
 from methods.kyu4.range_extraction import solution
 
 if __name__ == '__main__':
-    solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 22])
+    print(solution([-6,-3-1,3-5,7-11,14,15,17,18,19,20]))
--- methods/kyu4/range_extraction.py
+++ methods/kyu4/range_extraction.py
@@ -1,8 +1,6 @@
 def solution(args):
     args.sort()
-    s = []
-    start, end = True, False
-    i = 0
+    start, end, s, i = True, False, [], 0
     while i < len(args):
         try:
             if args[i] == args[i + 1] - 1 == args[i + 2] - 2 and start:
@@ -17,5 +15,13 @@ def solution(args):
                 s.append(str(args[i]))
                 start, end = True, False
         except IndexError:
-            continue
+            if not args[i] == args[-1] - 1 and not start:
+                s[-1] += str(args[i])
+                start, end = False, True
+            if not end:
+                s.append(str(args[i]))
+                start, end = True, False
+
         i += 1
+
+    return ','.join(map(str, s))
