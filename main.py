869243a9c483c8d2fb988126339afb7d9577cf9a
Danil <danrumu@gmail.com>
2022-04-21 23:42:06 +0300

    9 commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from methods.kuy5.rgb import rgb
+from methods.kuy5.simple_assembler import simple_assembler
 from methods.kuy5.validParentheses import valid_parentheses
 from methods.kyu7.find_next_square import find_next_square
 from methods.kyu6.spin_words import spin_words
@@ -6,6 +7,14 @@ from methods.kuy5.move_zeros import move_zeros
 from methods.kyu7.binary_array_to_number import binary_array_to_number
 from methods.kyu4.range_extraction import solution
 from methods.kyu4.snail import snail
+from methods.kyu7.square_digits import square_digits
 
 if __name__ == '__main__':
-    print(rgb(-20,275,125))
+    code = '''\
+mov a 5
+inc a
+dec a
+dec a
+jnz a -1
+inc a'''
+    print(simple_assembler(code))
---
+++ methods/kuy5/simple_assembler.py
@@ -0,0 +1,15 @@
+def mov(x, y):
+    pass
+
+def inc(x):
+    pass
+
+def dec(x):
+    pass
+
+def jnz(x, y):
+    pass
+
+def simple_assembler(program):
+	s = [i.split(' ') for i in program.split('\n')]
+	return s
